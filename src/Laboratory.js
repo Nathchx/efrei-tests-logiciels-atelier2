b7b37a2f1769245880d1c9d18b98fa6966b661f7
Nathan <nathan.chailleux@icloud.com>
2025-12-09 11:46:46 +0100

    TEST : init regular list - refactor

--- src/Laboratory.js
+++ src/Laboratory.js
@@ -1,21 +1,21 @@
 class Laboratory {
     constructor(substances) {
-        this.substanceList = [];
+        this.substanceList = new Set();
         
         if (!substances || substances.length === 0) {
             throw new Error("Substance list cannot be empty");
         }
         
         for (const s of substances) {
-            if (this.substanceList.includes(s)) {
+            if (this.substanceList.has(s)) {
                 throw new Error(`Duplicate substance: ${s}`);
             }
-            this.substanceList.push(s);
+            this.substanceList.add(s);
         }
     }
 
     getQuantity(substance) {
-        if (!this.substanceList.includes(substance)) {
+        if (!this.substanceList.has(substance)) {
             throw new Error(`Substance not found: ${substance}`);
         }
         return 0;
